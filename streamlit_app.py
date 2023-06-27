0d3534b7b5548734202ea7ff400feefb809c9845
finanazwa <137764298+finanazwa@users.noreply.github.com>
2023-06-27 19:58:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,39 @@
-
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-from datetime import datetime
+def load_journal():
+    # Load the journal data from a file or database
+    # For demonstration purposes, we'll use a pandas DataFrame
+    journal_data = pd.DataFrame(columns=["Date", "Entry"])
+    return journal_data
 
-# Function to get the date from user input
-def get_date():
-    while True:
-        date_str = input("Enter the due date (YYYY-MM-DD): ")
-        try:
-            date = datetime.strptime(date_str, "%Y-%m-%d")
-            return date
-        except ValueError:
-            print("Invalid date format. Please try again.")
+def save_journal(journal_data):
+    # Save the journal data to a file or database
+    # For demonstration purposes, we'll simply print the data
+    print(journal_data)
 
-# Function to get the priority from user input
-def get_priority():
-    while True:
-        priority = input("Enter the priority (important/not important): ")
-        if priority.lower() in ["important", "not important"]:
-            return priority.lower()
-        else:
-            print("Invalid priority. Please try again.")
+def main():
+    # Load existing journal data or create a new one
+    journal_data = load_journal()
 
-# Create a class for tasks
-class Task:
-    def __init__(self, description, due_date, priority):
-        self.description = description
-        self.due_date = due_date
-        self.priority = priority
+    # Title and introduction
+    st.title("Journal App")
+    st.write("Welcome to your personal journal!")
 
-    def __str__(self):
-        return f"{self.description} (Due: {self.due_date.strftime('%Y-%m-%d')}, Priority: {self.priority.capitalize()})"
+    # User input for new journal entry
+    entry_date = st.date_input("Date", value=pd.to_datetime("today").date())
+    entry_text = st.text_area("Entry", height=200)
 
-# Create an empty list to store tasks
-tasks = []
+    # Save the entry when the user clicks the button
+    if st.button("Save Entry"):
+        journal_data = journal_data.append({"Date": entry_date, "Entry": entry_text}, ignore_index=True)
+        save_journal(journal_data)
+        st.success("Entry saved successfully!")
 
-# Get the number of tasks from the user
-num_tasks = int(input("Enter the number of tasks: "))
+    # Display existing journal entries
+    st.subheader("Journal Entries")
+    st.dataframe(journal_data)
 
-# Prompt the user to input task details
-for i in range(1, num_tasks + 1):
-    print(f"\nTask {i}:")
-    description = input("Enter the task description: ")
-    due_date = get_date()
-    priority = get_priority()
-    tasks.append(Task(description, due_date, priority))
+if __name__ == "__main__":
+    main()
 
-# Sort tasks by due date and priority
-sorted_tasks = sorted(tasks, key=lambda x: (x.due_date, x.priority == "important"), reverse=False)
-
-# Print the sorted tasks
-print("\nSorted Tasks:")
-for task in sorted_tasks:
-    print(task)
